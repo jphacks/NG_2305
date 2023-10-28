a914ec9a715e8ecbf9a9bde695a67123e6cb1fee
Hikaru Aohara <hikaruaohara@icloud.com>
2023-10-28 09:14:11 +0900

    Xcodeプロジェクトを作成 .gitignoreを作成

---
+++ PredicTalk/PredicTalk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PredicTalk
+//
+//  Created by 青原光 on 2023/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PredicTalk/PredicTalk/PredicTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredicTalkApp.swift
+//  PredicTalk
+//
+//  Created by 青原光 on 2023/10/28.
+//
+
+import SwiftUI
+
+@main
+struct PredicTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
