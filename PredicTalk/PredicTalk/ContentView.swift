448d4afe2ca9923b0ff0cbb9598b2ff644a80cf2
Hikaru Aohara <hikaruaohara@icloud.com>
2023-10-29 11:50:36 +0900

    READMEを更新

--- PredicTalk/PredicTalk/ContentView.swift
+++ PredicTalk/PredicTalk/ContentView.swift
@@ -15,34 +15,45 @@ struct ContentView: View {
     let haptic = UIImpactFeedbackGenerator(style: .medium)
     
     var body: some View {
-        VStack(spacing: 0) {
-            ScrollView {
-                Group {
-                    Text("\(speechRecognizer.transcript) ") +
-                    Text(prediction)
-                        .foregroundColor(.secondary)
-                }
-                .font(.largeTitle)
-                .bold()
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                .onChange(of: speechRecognizer.transcript) { newTranscript in
-                    Task {
-                        do {
-                            if !newTranscript.isEmpty {
-                                try await prediction = APIRequest.shared.predict(sentence: newTranscript)
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                ScrollView(.horizontal) {
+                    ScrollViewReader { reader in
+                        Group {
+                            Text("\(speechRecognizer.transcript) ").foregroundColor(.white) +
+                            Text(prediction)
+                                .foregroundColor(.secondary)
+                        }
+                        .id(0)
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                        .onChange(of: speechRecognizer.transcript) { newTranscript in
+                            Task {
+                                do {
+                                    if !newTranscript.isEmpty {
+                                        try await prediction = APIRequest.shared.predict(sentence: newTranscript)
+                                    }
+                                } catch {
+                                    print(error)
+                                }
                             }
-                        } catch {
-                            print(error)
+                            withAnimation(.easeInOut) {
+                                reader.scrollTo(0)
+                            }
                         }
                     }
                 }
-            }
-            .onTapGesture {
-                haptic.impactOccurred()
-                isRecording.toggle()
-            }
-            .onChange(of: isRecording) { isRecording in
-                isRecording ? speechRecognizer.transcribe() : speechRecognizer.stopTranscribing()
+                .onTapGesture {
+                    haptic.impactOccurred()
+                    isRecording.toggle()
+                }
+                .onChange(of: isRecording) { isRecording in
+                    isRecording ? speechRecognizer.transcribe() : speechRecognizer.stopTranscribing()
+                }
             }
         }
     }
