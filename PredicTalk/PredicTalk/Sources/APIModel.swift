49d16f2e9e0d4c7661c73a1b260a50de59379fed
Hikaru Aohara <hikaruaohara@icloud.com>
2023-10-29 09:26:16 +0900

    APIレスポンスのパースモデルを作成

---
+++ PredicTalk/PredicTalk/Sources/APIModel.swift
@@ -0,0 +1,40 @@
+//
+//  APIModel.swift
+//  PredicTalk
+//
+//  Created by 青原光 on 2023/10/29.
+//
+
+import Foundation
+
+public struct ChatCompletion: Decodable {
+    public let id: String
+    public let choices: [Choice]
+    public let created: Int
+    public let model: String
+    public let object: String
+    public let usage: Usage
+}
+
+public struct Choice: Decodable {
+    public let finish_reason: String
+    public let index: Int
+    public let message: Message
+}
+
+public struct Message: Decodable {
+    public let content: String?
+    public let function_call: FunctionCall?
+    public let role: String
+}
+
+public struct FunctionCall: Decodable {
+    public let arguments: String
+    public let name: String
+}
+
+public struct Usage: Decodable {
+    public let completion_tokens: Int
+    public let prompt_tokens: Int
+    public let total_tokens: Int
+}
