9e6b85dc5fbd91236b685428c7221e83e62308d6
Asahi Ito <aiasahisun@gmail.com>
2023-11-08 13:58:44 +0900

    APIリクエストにTimer設定

--- PredicTalk/PredicTalk/ContentView.swift
+++ PredicTalk/PredicTalk/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @StateObject var speechRecognizer = SpeechRecognizer()
     @State private var prediction = ""
     @State private var isRecording = false
+    @State private var timer: Timer?
     
     let haptic = UIImpactFeedbackGenerator(style: .medium)
     
@@ -30,15 +31,18 @@ struct ContentView: View {
                         .font(.largeTitle)
                         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                         .onChange(of: speechRecognizer.transcript) { newTranscript in
-                            Task {
-                                do {
-                                    if !newTranscript.isEmpty {
-                                        try await prediction = APIRequest.shared.predict(sentence: newTranscript)
+                            timer?.invalidate()
+                            timer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: false) { timer in
+                                Task {
+                                    do {
+                                        if !speechRecognizer.transcript.isEmpty {
+                                            try await prediction = APIRequest.shared.predict(sentence: newTranscript)
+                                        }
+                                    } catch {
+                                        print(error)
                                     }
-                                } catch {
-                                    print(error)
                                 }
-                            }
+                           }
                         }
                     }
                 }
