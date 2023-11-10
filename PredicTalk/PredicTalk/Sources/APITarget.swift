e0264ac8fefff007a6f0a734db3407866f68ac6f
Hikaru Aohara <hikaruaohara@icloud.com>
2023-11-10 13:17:42 +0900

    ひらがな化APIを実装 OpenAIAPIKey.swiftは必要がなくなったため削除

--- PredicTalk/PredicTalk/Sources/APITarget.swift
+++ PredicTalk/PredicTalk/Sources/APITarget.swift
@@ -10,15 +10,26 @@ import Moya
 
 public enum APITarget: Decodable {
     case predict(sentence: String)
+    case toHiragana(sentence: String)
 }
 
 extension APITarget: TargetType {
     public var baseURL: URL {
-        return URL(string: "https://api.openai.com/v1")!
+        switch self {
+        case .predict:
+            return URL(string: "https://api.openai.com/v1")!
+        case .toHiragana:
+            return URL(string: "https://labs.goo.ne.jp/api")!
+        }
     }
     
     public var path: String {
-        return "/chat/completions"
+        switch self {
+        case .predict:
+            return "/chat/completions"
+        case .toHiragana:
+            return "/hiragana"
+        }
     }
     
     public var method: Moya.Method {
@@ -30,7 +41,7 @@ extension APITarget: TargetType {
         case .predict(let sentence):
             var data: [String: Any] = [:]
             
-            data["model"] = "gpt-3.5-turbo"
+            data["model"] = "gpt-4-1106-preview"
             
             let sysMsg = ["role": "system", "content": "Generate a continuation of the text entered. Also, please keep the sentences as short as possible. Make sure that the sentences you generate are natural when combined with the input sentences."]
             let usrMsg = ["role": "assistant", "content": sentence]
@@ -40,15 +51,29 @@ extension APITarget: TargetType {
             data["temperature"] = 0
             
             return .requestParameters(parameters: data, encoding: JSONEncoding.default)
+        case .toHiragana(let sentence):
+            let data: [String: Any] = ["app_id": APP_ID, "sentence": sentence, "output_type": "hiragana"]
+            return .requestParameters(parameters: data, encoding: JSONEncoding.default)
         }
     }
     
     public var sampleData: Data {
-        let url = Bundle.main.url(forResource: "MockResponse", withExtension: "json")!
-        return try! Data(contentsOf: url)
+        switch self {
+        case .predict:
+            let url = Bundle.main.url(forResource: "ChatResponse", withExtension: "json")!
+            return try! Data(contentsOf: url)
+        case .toHiragana:
+            let url = Bundle.main.url(forResource: "HiraganaResponse", withExtension: "json")!
+            return try! Data(contentsOf: url)
+        }
     }
     
     public var headers: [String : String]? {
-        return ["Authorization": "Bearer \(API_KEY)"]
+        switch self {
+        case .predict:
+            return ["Authorization": "Bearer \(API_KEY)"]
+        case .toHiragana:
+            return ["Content-Type": "application/json"]
+        }
     }
 }
