02e011d058d4cb9970e8e0062347b42f50b25ea1
Hikaru Aohara <hikaruaohara@icloud.com>
2023-11-16 02:00:38 +0900

    ARモードの画面を管理するARViewを追加 画面を横画面に固定するためのLandscapeLockedViewを追加

--- PredicTalk/PredicTalk/View/ARView.swift
+++ PredicTalk/PredicTalk/View/ARView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ARView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LandscapeLockedView()
+            Text("lotate")
+        }
     }
 }
 
